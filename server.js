596773844f881f7d0459030b9ce74620e14a987c
Satyam Singh <satyam010singh@gmail.com>
2017-03-14 21:23:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,15 +18,6 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db', function(req, res){
-    pool.query('SELECT * FROM test', function (err, result){
-       if(err){
-           res.status(500).send(err.toString());
-       } else {
-           res.send(JSON.stringify(result.rows));
-       }
-    });
-});
 
 app.get('/ui/index-style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index-style.css'));
@@ -134,7 +125,7 @@ app.get('/article/:pagename', function (req, res){
            res.status(500).send(err.toString()+req.params.pagename);
        } else {
            if(result.rows.length === 0){
-               res.status(404).send("Article "+req.params.pagename]+" Not Found!");
+               res.status(404).send("Article "+req.params.pagename+" Not Found!");
            } else {
                console.log(`result.rows.length >> result.rows[0]`);
                var articleData = result.rows[0];
