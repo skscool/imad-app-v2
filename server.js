600f4a36ecfc8cff6d852f44c38e352894a2fad7
Satyam Singh <satyam010singh@gmail.com>
2017-03-27 23:51:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -209,7 +209,8 @@ app.post('/login', function(req, res){
 });
 
 app.get('/check-login', function(req, res){
-   if(req.session.auth.userId){
+    res.send('test >> ' + req.session.auth.userId.toString());
+   if(req.session && req.session.auth && req.session.auth.userId){
        res.send('You are logged in '+ req.session.auth.userId.toString());
    } else{
        res.send('You are not logged in ');
