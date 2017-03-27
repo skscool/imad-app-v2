f7164e0889574b7c3c5ec1dcd6cdfbc33df3c984
Satyam Singh <satyam010singh@gmail.com>
2017-03-27 16:00:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,6 +163,30 @@ app.post('/create-user', function (req, res){
    });
 });
 
+app.post('/login', function(req, res){
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length === 0){
+               res.send(403).send('invalid credentials!');
+           }else{
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt);
+               if(hashedPassword === dbString){
+                   res.send('Credentials are correct');
+               }else{
+                   res.send(403).send('invalid credentials!');
+               }
+           }
+       }
+   });
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
