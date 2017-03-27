9a9bca2651f753e16e19129a79d6312c4ce4a931
Satyam Singh <satyam010singh@gmail.com>
2017-03-27 21:14:01 +0530

    [imad-console] Updates login.js

--- login.js
+++ login.js
@@ -21,5 +21,5 @@ function verify(){
   console.log(password);
   request.open('POST', 'http://skscool.imad.hasura-app.io/login',true);
   request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.Stringify({username: username, password: password}));
+  request.send(JSON.stringify({username: username, password: password}));
 }
