abb2dcfe32ee7b85a9b037bbce2abae7ad8aa5cd
Satyam Singh <satyam010singh@gmail.com>
2017-03-30 02:30:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.post('/login', function(req, res){
                var hashedPassword = hash(password, salt);
                if(hashedPassword === dbString){
                   //set the session
-                  req.session.auth = {userId: result.rows[0].id};
+                  req.session.auth = {userId: result.rows[0].username};
                   //set the cookie with sessionId, Internally maps to session object, Auth contain userId object
                    res.send('Credentials are correct');
                }else{
@@ -209,15 +209,7 @@ app.post('/login', function(req, res){
 });
 
 app.get('/check-login', function(req, res){
-    if(req.session.page_views){
-      req.session.page_views++;
-      res.send("You visited this page " + req.session.page_views + " times");
-   }else{
-      req.session.page_views = 1;
-      res.send("Welcome to this page for the first time!");
-   }
-   
-   
+
    if(req.session.auth.userId){
        res.send('You are logged in '+ req.session.auth.userId.toString());
    } else{
