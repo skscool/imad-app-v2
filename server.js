a37403c2541ade0a7dadd25befc5730ffca700ff
Satyam Singh <satyam010singh@gmail.com>
2017-03-30 02:25:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -209,6 +209,15 @@ app.post('/login', function(req, res){
 });
 
 app.get('/check-login', function(req, res){
+    if(req.session.page_views){
+      req.session.page_views++;
+      res.send("You visited this page " + req.session.page_views + " times");
+   }else{
+      req.session.page_views = 1;
+      res.send("Welcome to this page for the first time!");
+   }
+   
+   
    if(req.session.auth.userId){
        res.send('You are logged in '+ req.session.auth.userId.toString());
    } else{
